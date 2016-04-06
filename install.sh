f73f25024f435c734d9cfe5603ee9455200a7681
Nicolas Ledez <github@ledez.net>
2016-04-06 16:28:44 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ function install_prod() {
 	fi
 	[ ! -d virtualenv ] && virtualenv virtualenv
 	if [ requirements.txt -nt .requirements.txt.install ]; then
-		./virtualenv/bin/pip install -r requirements.txt
+		./virtualenv/bin/pip install -r requirements.txt && \
 		touch .requirements.txt.install
 	else
 		echo "Prod virtualenv uptodate"
@@ -21,7 +21,7 @@ function install_tests() {
 	fi
 	install_prod
 	if [ requirements-tests.txt -nt .requirements-tests.txt.install ]; then
-		./virtualenv/bin/pip install -r requirements-tests.txt
+		./virtualenv/bin/pip install -r requirements-tests.txt && \
 		touch .requirements-tests.txt.install
 	else
 		echo "Tests virtualenv uptodate"
@@ -35,8 +35,7 @@ function install_dev() {
 	fi
 	install_tests
 	if [ requirements-dev.txt -nt .requirements-dev.txt.install ]; then
-		./virtualenv/bin/pip install -r requirements-dev.txt
-		./virtualenv/bin/pip install -r requirements-dev.txt
+		./virtualenv/bin/pip install -r requirements-dev.txt && \
 		touch .requirements-dev.txt.install
 	else
 		echo "Dev virtualenv uptodate"
